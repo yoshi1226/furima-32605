4fefd99c537df1bb2ab9d3df5ad361bc7cf6a6d4
yoshi1226 <75423226+yoshi1226@users.noreply.github.com>
2021-02-03 20:56:58 +0900

    first_commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :destroy, :update]
+
   def index
-    
+    @items = Item.all
   end
 
   def new
