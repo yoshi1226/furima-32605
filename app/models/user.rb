a67308dd92e362119d05d8a52d3974e5605d73d1
yoshi1226 <75423226+yoshi1226@users.noreply.github.com>
2021-03-05 13:32:01 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-          validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+          validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
           with_options presence: true do
             validates :nickname
             validates :birthday
-            with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
-              validates :last_name
-              validates :first_name
-            end
-            with_options format:{with: /(?=.*[ア-ン])/} do
-              validates :last_name_kana
-              validates :first_name_kana
-            end
+          with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+            validates :last_name
+            validates :first_name
+          end
+          with_options format:{with: /(?=.*[ア-ン])/} do
+            validates :last_name_kana
+            validates :first_name_kana
           end
         end
+      end
