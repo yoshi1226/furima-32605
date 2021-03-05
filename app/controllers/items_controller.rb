53d511fa05a624c1cee0a2c554e282a281700b91
yoshi1226 <75423226+yoshi1226@users.noreply.github.com>
2021-03-05 12:02:54 +0900

    firstcommit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user_id
+    if current_user.id != @item.user_id ||  @item.purchase_record != nil
       return redirect_to root_path
     end
   end
